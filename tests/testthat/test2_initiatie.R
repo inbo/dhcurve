ea5a2611602c853c30fc80e67f3a2f58b20f518c
LOMMELEN <els.lommelen@inbo.be>
2017-08-31 15:28:48 +0200

    fout in testfunctie initiatie hersteld

--- tests/testthat/test2_initiatie.R
+++ tests/testthat/test2_initiatie.R
@@ -333,17 +333,17 @@ describe("initiatie", {
                    arrange(Rijnr))
   })
 
+  it("De bestandsnaam moet eindigen op .html", {
+    expect_error(initiatie(Dataset, Bestandsnaam = "Fout.htlm"),
+                 "De bestandnaam moet eindigen op '.html'")
+  })
+
   Dataset$Status <-
     ifelse(Dataset$DOMEIN_ID == "BosKlein", "Afgekeurd", "Niet gecontroleerd")
 
   it("Dataset mag geen afgekeurde gegevens bevatten", {
     expect_error(initiatie(Dataset),
                  "De kolom Status in de dataframe heeft niet voor alle records een geldige waarde.  Zorg dat enkel de waarden 'Niet gecontroleerd', 'Te controleren' en 'Goedgekeurd' voorkomen.") #nolint
-  })
-
-  it("De bestandsnaam moet eindigen op .html", {
-    expect_error(initiatie(Dataset, Bestandsnaam = "Fout.htlm"),
-                 "De bestandnaam moet eindigen op '.html'")
   })
 
   setwd(wd)
