4e24c4fb5265c84dd9a65bea4dfbd3d8ea69636b
ElsLommelen <els.lommelen@inbo.be>
2023-03-10 17:25:50 +0100

    en ook de bijhorende unittest ongedaan maken...

--- tests/testthat/test12_outputIVANHO.R
+++ tests/testthat/test12_outputIVANHO.R
@@ -38,17 +38,18 @@ describe("outputIVANHO", {
                    as.data.frame(., stringsAsFactors = FALSE),
                  data.frame(BMS = "testboom",
                             IDbms = 1,
-                            DOMEIN_ID = rep(c("HM", "LM"), 25),
-                            BOS_BHI = rep(c("HoogMinimum", "LaagMaximum"), 25),
-                            Omtrek = rep(seq(0.15, 2.55, 0.1), each = 2),
+                            DOMEIN_ID = c("HM", rep(c("HM", "LM"), 24)),
+                            BOS_BHI =
+                              c("HoogMinimum",
+                                rep(c("HoogMinimum", "LaagMaximum"), 24)),
+                            Omtrek =
+                              c(0.15, rep(seq(0.25, 2.55, 0.1), each = 2)),
                             stringsAsFactors = FALSE) %>%
                    mutate(
                      Hoogte =
                        ifelse(DOMEIN_ID == "HM",
                               20 + 7 * log(Omtrek) + 4 * log(Omtrek) ^ 2,
-                              ifelse(
-                                Omtrek == 0.15, 2.5,
-                                20 + 5 * log(Omtrek) - 6 * log(Omtrek) ^ 2)),
+                              20 + 5 * log(Omtrek) - 6 * log(Omtrek) ^ 2),
                      Modeltype = "basismodel"
                    ),
                  tolerance = 1)
@@ -66,18 +67,18 @@ describe("outputIVANHO", {
                  data.frame(
                    BMS = "andereboom",
                    IDbms = 2,
-                   DOMEIN_ID = rep(c("HM", "LM"), each = 25),
-                   BOS_BHI = rep(c("HoogMinimum", "LaagMaximum"), each = 25),
-                   Omtrek = rep(seq(0.15, 2.55, 0.1), 2),
+                   DOMEIN_ID = c("HM", rep(c("HM", "LM"), each = 24)),
+                   BOS_BHI =
+                     c("HoogMinimum",
+                       rep(c("HoogMinimum", "LaagMaximum"), each = 24)),
+                   Omtrek = c(0.15, rep(seq(0.25, 2.55, 0.1), 2)),
                    stringsAsFactors = FALSE
                  ) %>%
                    mutate(
                      Hoogte =
                        ifelse(DOMEIN_ID == "HM",
                               20 + 7 * log(Omtrek) + 4 * log(Omtrek) ^ 2,
-                              ifelse(
-                                Omtrek == 0.15, 2.5,
-                                20 + 5 * log(Omtrek) - 6 * log(Omtrek) ^ 2)),
+                              20 + 5 * log(Omtrek) - 6 * log(Omtrek) ^ 2),
                      Modeltype = "lokaal model"
                    ),
                  tolerance = 1)
@@ -118,12 +119,10 @@ describe("outputIVANHO", {
                             IDbms = 1,
                             DOMEIN_ID = "Klein",
                             BOS_BHI = "DOMEIN_Klein",
-                            Omtrek = seq(0.25, 2.55, 0.1),
+                            Omtrek = seq(0.45, 2.55, 0.1),
                             stringsAsFactors = FALSE) %>%
                    mutate(
-                     Hoogte =
-                       ifelse(Omtrek < 0.4, 2.5,
-                              15 + 15 * log(Omtrek) + log(Omtrek) ^ 2),
+                     Hoogte = 15 + 15 * log(Omtrek) + log(Omtrek) ^ 2,
                      Modeltype = "afgeleid model"
                    ),
                  tolerance = 1)
